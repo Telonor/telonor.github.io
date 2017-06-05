4a2c1011d81cc1e386dfca479e161fabbf4c42d7
= <=>
2017-06-05 13:29:58 +0300

    Update info.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,9 @@
-/**
- * Created by Telonor on 6/5/17.
- */
+var bday = '02-25-1989';
+
+function calculateAge(birthday) { // birthday is a date
+    var ageDifMs = Date.now() - birthday.getTime();
+    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+document.getElementById('age').innerHTML = calculateAge(new Date(bday));
