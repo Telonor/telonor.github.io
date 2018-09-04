ec5fa00d3772c4e2afc05587c20fa5de20640a28
Anton Viievskyi (Telonor) <viievskyi.anton@gmail.com>
2018-09-04 17:33:35 +0300

    Restart with partial move to CRA.

--- assets/js/index.js
+++
@@ -1,9 +0,0 @@
-var bday = '02-25-1989';
-
-function calculateAge(birthday) { // birthday is a date
-    var ageDifMs = Date.now() - birthday.getTime();
-    var ageDate = new Date(ageDifMs); // miliseconds from epoch
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
-}
-
-document.getElementById('age').innerHTML = calculateAge(new Date(bday));
